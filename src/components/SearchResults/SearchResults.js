740a27cfb54981e21ea0160f0401a7cc9d0cf9d1
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-12-02 20:02:35 +0545

    initialised SearchResults.js and added css files

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+import store from "../../store/store";
+import './SearchResults.css';
+import { selectSearchResults } from "../../features/searchSlice";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+import { ResultsLoading } from "./ResultsLoading";
+
+export const SearchResults = () => {
+    
+}
