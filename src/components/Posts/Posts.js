6369d55f2483189fa21752795cf40e276ae1fdc9
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-12-23 19:29:05 +0530

    Removed one PostLoading from Posts.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -32,7 +32,6 @@ export const Posts = () => {
                     <PostLoading />
                     <PostLoading />
                     <PostLoading />
-                    <PostLoading />
                 </ul>
             </div>
         )
