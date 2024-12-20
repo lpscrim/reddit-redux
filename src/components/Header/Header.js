f500d5ae599f3f145e0ba3faa8390962a3aa0593
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-20 16:46:10 +0000

    updated favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -33,7 +33,7 @@ export const Header = () => {
   return (
     <div className="header">
       <div className="title">
-        <img src="logo.png" alt="logo of reddit with bread" height="50px" width="auto"/>
+        <img src="favicon.png" alt="logo of reddit with bread" height="50px" width="auto"/>
         <img src="Bread.png" alt="website name in bread" height="200px" width="auto"/>
       </div>
       <form onSubmit={handleSubmit} data-testid="search-form">
